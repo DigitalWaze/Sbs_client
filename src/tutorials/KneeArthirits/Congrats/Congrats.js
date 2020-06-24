e5e51d5db4770be821f598d916ce67ee4b5014b5
Abdul Haseeb <abdulhaseeb13mar@gmail.com>
2020-06-24 17:39:31 +0500

    overview and congrats page continue button adjusted

--- src/tutorials/KneeArthirits/Congrats/Congrats.js
+++ src/tutorials/KneeArthirits/Congrats/Congrats.js
@@ -16,41 +16,63 @@ class Congrats extends Component {
   render() {
     return (
       <div id="Tutorials_Congrats_Main_Div">
-        <div id="Tutorials_Congrats_Text_Wrapper">
-          <div id="Tutorials_Congrats_Heading1_Div">Congratulations</div>
-          <div id="Tutorials_Congrats_Neon_Line"></div>
-          <div id="Tutorials_Congrats_Heading2_Div">
-            You have finished the Knee Arthritis Education Module.
+        <div id="Tutorials_Congrats_Content_Wrapper">
+          <div id="Tutorials_Congrats_Center_Wrapper">
+            <div id="Tutorials_Congrats_Inner_Content_Wrapper">
+              <div id="Tutorials_Congrats_Text_Wrapper">
+                <div>
+                  <div id="Tutorials_Congrats_Heading1_Div">
+                    Congratulations
+                  </div>
+                  <div id="Tutorials_Congrats_Neon_Line"></div>
+                  <div id="Tutorials_Congrats_Heading2_Div">
+                    You have finished the Knee Arthritis Education Module.
+                  </div>
+                  <div id="Tutorials_Congrats_Heading2_Div">
+                    You are now ready to start the X-ray Matching Education
+                    Module.
+                  </div>
+                </div>
+              </div>
+              <div id="Tutorials_Congrats_Image_div">
+                <img
+                  src={Bone1Image}
+                  alt="SBS"
+                  id="Tutorials_Congrats_Image_Bone"
+                />
+              </div>
+            </div>
+            <div id="Tutorials_Congrats_Next_Button_Div">
+              <Button
+                id="Tutorials_Congrats_Next_Button"
+                variant="contained"
+                onClick={() => {
+                  this.context.history.push(
+                    "./x-ray-matching-moderate-selected"
+                  );
+                }}
+              >
+                {" "}
+                back{" "}
+              </Button>
+              <Button
+                id="Tutorials_Congrats_Next_Button"
+                variant="contained"
+                onClick={() => {
+                  this.context.setCookie(
+                    "tutorial-" + this.context.state.user_id,
+                    32
+                  );
+                  this.context.history.push(
+                    "/tutorials/matching-education/welcome"
+                  );
+                }}
+              >
+                {" "}
+                Continue{" "}
+              </Button>
+            </div>
           </div>
-          <div id="Tutorials_Congrats_Heading2_Div">
-            You are now ready to start the X-ray Matching Education Module.
-          </div>
-          <div id="Tutorials_Congrats_Next_Button_Div">
-            <Button
-              id="Tutorials_Congrats_Next_Button"
-              variant="contained"
-              onClick={() => {
-                this.context.history.push("./x-ray-matching-moderate-selected");
-              }}
-            >
-              {" "}
-              back{" "}
-            </Button>
-            <Button
-              id="Tutorials_Congrats_Next_Button"
-              variant="contained"
-              onClick={() => {
-                this.context.setCookie("tutorial-" + this.context.state.user_id,32);
-                this.context.history.push("/tutorials/matching-education/welcome");
-              }}
-            >
-              {" "}
-              Continue{" "}
-            </Button>
-          </div>
-        </div>
-        <div id="Tutorials_Congrats_Image_div">
-          <img src={Bone1Image} alt="SBS" id="Tutorials_Congrats_Image_Bone" />
         </div>
       </div>
     );
