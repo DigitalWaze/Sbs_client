4b2f9bf881a1f57eb2a2a3b501fe057b3b4be502
ShahzadaAhmed <shahzada008@hotmail.com>
2021-02-02 17:06:47 +0500

    JPR File Updated

--- src/evaluation/recommendedCarePathway/JPR.jsx
+++ src/evaluation/recommendedCarePathway/JPR.jsx
@@ -1,12 +1,101 @@
 import React, { Component } from 'react';
+import "./JPR.css";
+import Button from "@material-ui/core/Button";
+
+const comparments = [
+    { name: 'Normal to Slight', id: '1' },
+    { name: 'Moderate', id: '2' },
+    { name: 'Near End Stage', id: '3' },
+    { name: 'End Stage', id: '4' }
+];
+
 class JPR extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = {}
     }
-    render() { 
-        return ( <div></div> );
+    render() {
+        const replacement = this.props.Replacement;
+        return (
+            // Main container
+            <div id="JointSummary-main-box">
+                <div id="JointSummary-box-1">
+                    <h1 id="Knee-Replacement-heading" style={{ marginBottom: "20px" }}>
+                        {this.props.Joint_Name}: <br /> Partial Repalcement
+                    </h1>
+
+                    <div id="JointSummary-row-1">
+                        <div style={{ marginLeft: "20px" }}>
+                            <h3 id="JointSummary-part-1-heading"> Medial </h3>
+                            <div id="JointSummary-part-1" style={{ backgroundColor: this.props.Compartment1.toString() === "1" ? "#F26E82" : "#B3D89B" }}>
+                                {comparments.find((comp) => comp.id.toString() === this.props.Compartment1.toString()).name}
+                            </div>
+                        </div>
+
+                        <div>
+                            <h3 id="JointSummary-part-1-heading">  Lateral </h3>
+                            <div id="JointSummary-part-1" style={{ backgroundColor: this.props.Compartment2.toString() === "2" ? "#B4D99C" : "#B3D89B" }}>
+                                {comparments.find((comp) => comp.id.toString() === this.props.Compartment2.toString()).name}
+                            </div>
+                        </div>
+
+                        <div>
+                            <h3 id="JointSummary-part-1-heading"> Kneecap </h3>
+                            <div id="JointSummary-part-1" style={{ backgroundColor: this.props.Compartment3.toString() === "3" ? "#FAF075" : "#B3D89B" }}>
+                                {comparments.find((comp) => comp.id.toString() === this.props.Compartment3.toString()).name}
+                            </div>
+                        </div>
+
+                        <div id="JointSummary-part-last-box">
+                            <h3 id="JointSummary-part-1-heading">  KOOS </h3>
+                            <div id="JointSummary-part-4" style={{ backgroundColor: "#F26E82" }}>
+                                {this.props.Score}
+                            </div>
+                        </div>
+                    </div>
+
+                    <h2 id="Knee-Replacement">
+                        SBS Recomendation:
+                    </h2>
+
+                    {/* Row: 2 */}
+                    <div id="Joint-Treatment-Options-box-2">
+                        <h2 id="Joint-Treatment-Options-header" className="JPR-header"> Partial Replacement </h2>
+
+                        <div>
+                            <div id="Knee-Replacement-small-box-1" className="JPR-footer-box1" style={{ opacity: replacement === "Medial" ? 1 : 0.5 }}> Medial </div>
+
+                            <div id="Knee-Replacement-small-box-1" className="JPR-footer-box2" style={{ opacity: replacement === "Lateral" ? 1 : 0.5 }}> Lateral </div>
+
+                            <div id="Knee-Replacement-small-box-1" className="JPR-footer-box3" style={{ opacity: replacement === "Lateral" ? 1 : 0.5 }}> Kneecap </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div id="RCPE-Welcome-box-2">
+                    <div id="RCPE-Welcome-footer-div1">
+                        <Button
+                            id="RCPE-Welcome-btn-1"
+                            variant="contained"
+                            onClick={this.props.handleBackClick}
+                        >
+                            {" "} Back {" "}
+                        </Button>
+                    </div>
+
+                    <div id="RCPE-Welcome-footer-div3">
+                        <Button
+                            id="JointSummary-btn-2"
+                            variant="contained"
+                            onClick={this.props.handleNextClick}
+                        >
+                            {" "} Confirm {" "}
+                        </Button>
+                    </div>
+                </div>
+            </div>
+        );
     }
 }
- 
+
 export default JPR;
